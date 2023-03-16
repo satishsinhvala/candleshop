69af7c481a146bf569a41d555ef8a6be487167ae
satishsinhvala <satishsinhvala1111@gmail.com>
2023-03-16 13:32:30 +0530

    update

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	// $('#dismiss, .overlay').on('click', function () {
+	// 	$('.menu-icon').removeClass('active');
+	// 	$('#sidebar').removeClass('active');
+	// 	$('.overlay').removeClass('active');
+	// });
+  
+	$('.icon').on('click', function () {
+		$('.topnav').addClass('active');
+		// $('.overlay').addClass('active');
+		// $('.menu-icon').addClass('active');
+	});
+});
